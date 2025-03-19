3fd0c7ed25a2f42d8230f3efb9a51b87a81b17bc
Artzski <74048354+Artzski@users.noreply.github.com>
2025-03-19 10:59:02 +0000

    Room working, Cars working

---
+++ Assets/CarScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 30f;
+    private Rigidbody rb;
+    public AK.Wwise.Event carScreech;
+    public AK.Wwise.Event carNoiseStart;
+    public AK.Wwise.Event carNoiseStop;
+    public bool playerDetected = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        rb.velocity = rb.velocity.normalized * speed;
+        if (playerDetected == true)
+        {
+            speed = 0f;
+            Debug.Log("speed is 0");
+        }
+        if (playerDetected == false)
+        {
+            speed = 30f;
+            rb.velocity = transform.forward * speed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("crash");
+            carNoiseStop.Post(gameObject);
+            carScreech.Post(gameObject);
+
+            playerDetected = true;
+        }
+        if (other.CompareTag("CarDespawnWall"))
+        {
+            Destroy(gameObject, 3);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            carNoiseStart.Post(gameObject);
+            playerDetected = false;
+        }
+    }
+}
---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject car;
+    [SerializeField] private float timer;
+    [SerializeField] private int randInt;
+    void Start()
+    {
+        timer = 0;
+        Instantiate(car, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            randInt = Random.Range(0, 200);
+            if (randInt == 0)
+            {
+                Instantiate(car, transform.position, transform.rotation);
+                timer = 1.5f;
+            }
+        }
+        timer = Mathf.Max(0, timer - Time.deltaTime);
+    }
+
+
+}
