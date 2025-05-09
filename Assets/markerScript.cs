93fdd1d8a871761a63408ac920365272cd5d20d7
Artzski <jack.thatcher12@gmail.com>
2025-05-09 17:47:11 +0100

    Markers Script
    Moves the markers and will play voicelines when i have them

---
+++ Assets/markerScript.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class markerScript : MonoBehaviour
+{
+    public AK.Wwise.Event voiceLine;
+    public AK.Wwise.Event voiceLine2;
+    public AK.Wwise.Event idleLine;
+    [SerializeField] private GameObject marker;
+    [SerializeField] private GameObject marker2;
+    [SerializeField] private float idleVoiceTimer;
+    [SerializeField] private bool playedOnce = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (playedOnce == false)
+            {
+                marker.SetActive(true);
+                this.gameObject.SetActive(false);
+                playedOnce = true;
+            }
+            else if (playedOnce == true)
+            {
+                marker2.SetActive(true);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((this.gameObject.activeSelf == true) && (idleVoiceTimer == 0))
+        {
+            idleLine.Post(gameObject);
+            idleVoiceTimer = 5;
+        }
+        idleVoiceTimer = Mathf.Max(0, idleVoiceTimer - Time.deltaTime);
+    }
+}
